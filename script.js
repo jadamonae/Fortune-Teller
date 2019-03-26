35e59ab5693ecaf420bb2c97f32cdf9aa42aad0a
jadamonae <43444196+jadamonae@users.noreply.github.com>
2019-03-26 09:29:51 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+$("button").click(function() {
+var train = $("train").val();
+parseInt(train);
+var nails = $("nails").val();
+var phone = $("phone").val();
+var 
+$(".future").text("You will take the train");
+});
